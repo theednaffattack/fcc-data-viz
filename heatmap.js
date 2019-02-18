c02945b6310465da82429ad15eb97e6affb25d98
eddie naff <eddienaff@eddies-i7-MBP.local>
2019-02-18 12:22:06 -0800

    Change css and xscaling

--- heatmap.js
+++ heatmap.js
@@ -6,13 +6,12 @@ function makeHeatMap(error, heatData) {
   if (error)
     console.error("ERROR LOADING DATA: \n" + JSON.stringify(error.message));
 
-  console.log(5 * Math.ceil(heatData.monthlyVariance.length / 12));
   const margin = { top: 50, right: 0, bottom: 100, left: 30 };
   const width = 960 - margin.left - margin.right;
   const height = 430 - margin.top - margin.bottom;
   const gridSize = Math.floor(heatData.monthlyVariance.length);
-  console.log("gridSize");
-  console.log(gridSize);
+
+  const itemSize = 10;
   const legendElementWidth = gridSize * 2;
   const buckets = 9;
   const colors = [
@@ -46,60 +45,46 @@ function makeHeatMap(error, heatData) {
 
   // X
   const xExtent = d3.extent(heatData.monthlyVariance, d => {
-    return d.year;
+    var date = new Date();
+
+    date.setUTCFullYear(d.year);
+    return date;
   });
 
-  // const xAxis = d3.svg.axis.scale(xScale).orient("bottom");
-  //ordinal scale
-  // var xScale = d3.scale
-  //   .ordinal()
-  //   .domain(
-  //     data.monthlyVariance.map(function(val) {
-  //       return val.year;
-  //     })
-  //   )
-  //   .rangeRoundBands([0, width], 0, 0);
-
   const xScale = d3
-    .scaleOrdinal()
+    .scaleTime()
     .domain(xExtent)
     .range([0, width]);
 
   const xAxis = d3
     .axisBottom()
     .scale(xScale)
-    .tickValues((d, i) => {
-      console.log("look at d");
-      console.log(d);
-      d.filter(x => {
-        console.log("x");
-        console.log(x);
-        x % 10 === 0;
-      });
-    })
-    .tickFormat(d3.format("d"));
+    .tickFormat(d3.timeFormat("%Y"));
 
   // Y
-  const yExtent = d3.extent([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
 
+  let y_first = new Date();
+  y_first.setMonth(-1);
+  let y_last = new Date();
+  y_last.setMonth(11);
+
+  let makeExtent = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(x => {
+    let tempDate = new Date(0);
+    tempDate.setUTCMonth(x);
+    return tempDate;
+  });
+
+  const yExtent = d3.extent(makeExtent);
   const yScale = d3
-    .scaleOrdinal()
+    .scaleTime()
     .domain(yExtent) // input; yExtent[1] = max
     .range([0, height]); // output
 
   const yAxis = d3
     .axisLeft()
-    // .tickFormat()
-    .scale(yScale);
+    .scale(yScale)
+    .tickFormat(d3.timeFormat("%b"));
 
-  // const yAxis = d3.svg.axis
-  //   .scale(yScale)
-  //   .orient("left")
-  //   .tick(12);
-
-  //   console.log(xExtent);
-  //   console.log(yExtent);
-  //   console.log(new Set(heatData.monthlyVariance));
   var svg = d3
     .select("#heatmapContainer")
     .append("svg")
@@ -182,14 +167,22 @@ function makeHeatMap(error, heatData) {
       })
     ])
     .range(colors);
-  // console.log(colorScale(85));
 
   ////////////////
 
+  // let the width of each heat box equal...
+  // the width minus margin left minus margin right...
+  // divided by the length of the data set (number of years depicted)...
+  // divided by 12
+  let horizontalMapTile =
+    (width - margin.left - margin.right) / (gridSize / 12);
+  let verticalMapTile = (height - margin.top - margin.bottom) / 12;
+
+  let min = d3.min(heatData.monthlyVariance, d => d.year);
   //map
   svg
     .append("g")
-    .classed("map", true)
+    // .classed("map", true)
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
     .selectAll("rect")
     .data(heatData.monthlyVariance)
@@ -205,152 +198,65 @@ function makeHeatMap(error, heatData) {
     .attr("data-temp", function(d) {
       return heatData.baseTemperature + d.variance;
     })
-    .attr("x", (d, i) => {
-      console.log("xScale(d.year)");
+
+    // returns a value : Integer
+    .attr("x", function(d, i) {
+      let x = d.year - xExtent[0].getFullYear();
+      // console.log(xExtent[0].getFullYear());
+      // console.log("x * horizontalMapTile + margin.left");
+      // console.log(horizontalMapTile);
+      console.log(x * horizontalMapTile);
       console.log(xScale(d.year));
-      return xScale(d.year);
+      console.log(x);
+      console.log(d.year);
+      // console.log(x * horizontalMapTile + margin.left);
+      // console.log(verticalMapTile);
+      // console.log(verticalMapTile);
+      // console.log(d.year);
+      return xScale(d.year) + margin.left;
     })
-    .attr("y", (d, i) => yScale(d.month))
-    .attr("width", (d, i) => 1)
-    .attr("height", (d, i) => 10)
-    // .attr({
-    //   x: function(d, i) {
-    //     return xScale(d.year);
-    //   },
-    //   y: function(d, i) {
-    //     return yScale(d.month);
-    //   },
-    //   width: function(d, i) {
-    //     return xScale.rangeBand(d.year);
-    //   },
-    //   height: function(d, i) {
-    //     return yScale.rangeBand(d.month);
-    //   }
+
+    // // returns undefined
+    // .attr("x", (d, i) => {
+    //   let x = d.year - xExtent[0];
+    //   console.log("x * horizontalMapTile + margin.left");
+    //   console.log(d.Year);
+    //   return x * horizontalMapTile + margin.left;
     // })
-    // .attr("fill", function(d, i) {
-    //   return legendThreshold(heatData.baseTemperature + d.variance);
-    // })
-    .on("mouseover", function(d) {
-      var date = new Date(d.year, d.month);
-      var str =
-        "<span class='date'>" +
-        d3.time.format("%Y - %B")(date) +
-        "</span>" +
-        "<br />" +
-        "<span class='temperature'>" +
-        d3.format(".1f")(heatData.baseTemperature + d.variance) +
-        "&#8451;" +
-        "</span>" +
-        "<br />" +
-        "<span class='variance'>" +
-        d3.format("+.1f")(d.variance) +
-        "&#8451;" +
-        "</span>";
-      tip.attr("data-year", d.year);
-      tip.show(str);
-    });
-  // .on("mouseout", tip.hide);
+    .attr("y", (d, i) => {
+      // console.log("verticalMapTile");
+      // console.log(verticalMapTile);
 
-  ////////////////
+      // console.log(
+      //   height -
+      //     (margin.bottom + verticalMapTile) -
+      //     (d.month - 1) * verticalMapTile
+      // );
+      return i + verticalMapTile;
+    })
 
-  // var cards = svg
-  //   .selectAll(".year")
-  //   .data(heatData.monthlyVariance, function(d) {
-  //     return d.year;
-  //   });
+    // .attr("y", (d, i) => d.month)
+    .attr("width", horizontalMapTile)
+    .attr("height", verticalMapTile);
 
-  // cards.append("title");
-  // console.log("gridSize");
-  // console.log(Math.round((width / gridSize) * 10));
-  // cards
-  //   .enter()
-  //   .append("rect")
-  //   .attr("x", function(d, i) {
-  //     // console.log(Math.round((width / gridSize) * 10));
-  //     console.log(d.variance);
-  //     console.log(colorScale(d.variance));
-  //     return Math.round((width / gridSize) * 10);
-  //   })
-  //   .attr("y", function(d) {
-  //     return (d.month - 1) * gridSize;
-  //   })
-  //   .attr("rx", (d, i) => i * 3)
-  //   .attr("ry", 0)
-  //   .attr("class", "year bordered")
-  //   .attr("width", gridSize)
-  //   .attr("height", gridSize)
-  //   .style("fill", d => colorScale(d.variance));
-
-  // cards
-  //   .transition()
-  //   .duration(1000)
-  //   .style("fill", function(d) {
-  //     return colorScale(d.variance);
-  //   });
-
-  // cards.select("title").text(function(d) {
-  //   return d.value;
+  // .on("mouseover", function(d) {
+  //   var date = new Date(d.year, d.month);
+  //   var str =
+  //     "<span class='date'>" +
+  //     d3.time.format("%Y - %B")(date) +
+  //     "</span>" +
+  //     "<br />" +
+  //     "<span class='temperature'>" +
+  //     d3.format(".1f")(heatData.baseTemperature + d.variance) +
+  //     "&#8451;" +
+  //     "</span>" +
+  //     "<br />" +
+  //     "<span class='variance'>" +
+  //     d3.format("+.1f")(d.variance) +
+  //     "&#8451;" +
+  //     "</span>";
+  //   tip.attr("data-year", d.year);
+  //   tip.show(str);
   // });
-
-  // cards.exit().remove();
-
-  // var legend = svg
-  //   .selectAll(".legend")
-  //   .data([0].concat(colorScale.quantiles()), function(d) {
-  //     console.log("d");
-  //     console.log(d);
-  //     return d;
-  //   });
-
-  // legend
-  //   .enter()
-  //   .append("g")
-  //   .attr("class", "legend");
-
-  // legend
-  //   .append("rect")
-  //   .attr("x", function(d, i) {
-  //     return 30 * i;
-  //   })
-  //   .attr("y", height)
-  //   .attr("width", (d, i) => i * 30) // legendElementWidth
-  //   .attr("height", 40) // gridSize / 2
-  //   .style("fill", function(d, i) {
-  //     return colors[i];
-  //   });
-
-  // legend
-  //   .append("text")
-  //   .attr("class", "mono")
-  //   .text(function(d) {
-  //     return "≥ " + Math.round(d);
-  //   })
-  //   .attr("x", function(d, i) {
-  //     return legendElementWidth * i;
-  //   })
-  //   .attr("y", height + gridSize);
-
-  // legend.exit().remove();
-  // }
-  //   );
-  // };
-
-  //   heatmapChart(datasets[0]);
-
-  //   var datasetpicker = d3
-  //     .select("#dataset-picker")
-  //     .selectAll(".dataset-button")
-  //     .data(datasets);
-
-  //   datasetpicker
-  //     .enter()
-  //     .append("input")
-  //     .attr("value", function(d) {
-  //       return "Dataset " + d;
-  //     })
-  //     .attr("type", "button")
-  //     .attr("class", "dataset-button")
-  //     .on("click", function(d) {
-  //       heatmapChart(d);
-  //     });
+  // .on("mouseout", tip.hide);
 }
